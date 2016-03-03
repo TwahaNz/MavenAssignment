88e67a406a01759b7672fe08c59e7b8ac0438fca
Twahanz <Twahanz@gmail.com>
2016-03-03 13:50:50 +0200

    Add Class

---
+++  MavenExercise/src/main/java/com/tnz/app/Maths.java
@@ -0,0 +1,26 @@
+package com.tnz.app;
+
+/**
+ * Created by student on 2016/03/03.
+ */
+public class Maths {
+
+
+    private int num;
+
+    public Maths() {
+    }
+
+    public Maths(int num){
+        this.num = num;
+    }
+
+
+    public float getFloat() {
+        return 10;
+    }
+
+    public long getInt() {
+        return 23;
+    }
+}
